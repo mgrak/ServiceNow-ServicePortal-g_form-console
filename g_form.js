39b0c1e851637dc84090c5d52b476c4607bdf9fd
Michael Rakowski <37604982+mgrak@users.noreply.github.com>
2024-04-25 16:57:27 +0000

    Update g_form.js
    prettify

--- g_form.js
+++ g_form.js
@@ -1,6 +1,6 @@
 var scope, g_form;
 $.find("input").every(el => {
-    if (g_form){
+    if (g_form) {
         console.log('g_form already exists');
         return false;
     }
@@ -9,10 +9,10 @@ $.find("input").every(el => {
         scope = angular.element($('#' + el.id)).scope();
         g_form = scope.$parent.$parent.$parent.getGlideForm();
     } catch (e) {}
-    if (g_form){
+    if (g_form) {
         console.log('Was able to grab element id  "' + el.id + '"');
-       return false;
+        return false;
     }
-	console.log('Was not able to find angular element containing g_form');
+    console.log('Was not able to find angular element containing g_form');
     return true;
 });
